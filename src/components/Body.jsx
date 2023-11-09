1b3c7e97676700b74dba4e55ab2f4376953e6223
Oscar Castañeda <109644116+OscarMoscar198@users.noreply.github.com>
2023-11-09 05:30:48 -0600

    First version
    Visual changes still non-functional

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,29 +1,113 @@
-import React from 'react'
-import BodyImg from '../assets/img/chiapaneca.jpeg'
+import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
 
-export default function Body() {
+export default function RegistrationForm() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegistration = () => {
+    // Lógica para manejar el registro
+    console.log('Usuario:', username);
+    console.log('Correo electrónico:', email);
+    console.log('Contraseña:', password);
+    console.log('Confirmar contraseña:', confirmPassword);
+  };
+
+  const handleGoogleLogin = () => {
+    // Lógica para iniciar sesión con Google
+    console.log('Iniciar sesión con Google');
+  };
+
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+    <div className="bg-white min-h-screen flex items-center justify-center">
+      <div className="w-2/4 p-8 bg-[#171717] rounded-md shadow-md">
+        <h2 className="text-3xl font-semibold mb-4 text-white text-center">Create Account</h2>
+        {/* Formulario de registro */}
+        <form className="flex flex-col items-center space-y-5">
+          <div className="w-full max-w-lg">
+            <label htmlFor="username" className="block text-sm font-normal text-white">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full h-8 p-2 rounded-sm"
+            />
+          </div>
 
-      {/* Imagen en el centro a la izquierda */}
-      <div className="w-1/2 p-4 text-center">
-        <img
-          src={BodyImg}
-          className="w-full p-20 shadow-sm" // Ajusta el tamaño máximo de la imagen
-        />
+          <div className="w-full max-w-lg">
+            <label htmlFor="email" className="block text-sm font-normal text-white">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full h-8 p-2 rounded-sm"
+            />
+          </div>
+
+          <div className="w-full max-w-lg">
+            <label htmlFor="password" className="block text-sm font-normal text-white">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full h-8 p-2 rounded-sm"
+            />
+          </div>
+
+          <div className="w-full max-w-lg">
+            <label htmlFor="confirmPassword" className="block text-sm font-normal text-white">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full h-8 p-2 rounded-sm"
+            />
+          </div>
+
+          <button
+            type="button"
+            onClick={handleRegistration}
+            className="bg-[#09724A] text-white px-4 mt-28 py-2 rounded-md w-4/12 flex items-center justify-center hover:bg-green-700 transition duration-150 ease-in"
+          >
+            Sign up
+          </button>
+
+          <div className="flex items-center w-full">
+            <hr className="flex-1 border-t border-white" />
+            <span className="mx-2 text-sm text-white">Or</span>
+            <hr className="flex-1 border-t border-white" />
+          </div>
+
+          {/* Opción de iniciar sesión con Google */}
+          <button
+            type="button"
+            onClick={handleGoogleLogin}
+            className="bg-white text-black px-4 py-2 rounded-2xl w-2/4 flex items-center justify-center hover:bg-gray-200 transition duration-150 ease-in"
+          >
+            <FcGoogle size={20} className="mr-2" />
+            Continue with Google
+          </button>
+          <a className='mt-6 mx-2 text-white'>Already have an account?</a>
+        </form>
       </div>
-
-      {/* Texto en el centro a la derecha */}
-      <div className="w-1/2 p-4 text-white">
-        <h2 className="text-3xl font-bold mb-4">EXPLORA NFT</h2>
-        <p className="mb-4 w-2/3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
-        </p>
-        <button className="bg-green-800 text-white px-4 py-2  w-36 rounded-md hover:bg-green-700 transition duration-150 ease-in">
-          EXPLORAR
-        </button>
-      </div>
-
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../assets/icon/quetz.png';
-import { BiWallet, BiCartAlt, BiSearch } from 'react-icons/bi';
 
 export default function Header() {
   return (
@@ -14,30 +13,9 @@ export default function Header() {
         />
         <span className="text-white text-2xl font-semibold">QUETZ</span>
       </div>
-
-      {/* Buscador */}
-      <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="BUSCAR"
-          className="rounded-l-sm p-2 w-96 focus:outline-none text-black"
-        />
-        <button className="bg-green-800 text-white p-2 w-14 rounded-r-sm flex items-center justify-center hover:bg-green-700 transition duration-150 ease-in">
-          <BiSearch size={25} />
-        </button>
-      </div>
-
       {/* Botones estilizados */}
-      <div className="space-x-14">
-        <button className="hover:bg-neutral-700 text-white px-4 py-2 rounded-sm text-sm transition duration-150 ease-in">EXPLORAR</button>
-        <button className="hover:bg-neutral-700 text-white px-4 py-2 rounded-sm text-sm transition duration-150 ease-in">EXPLORAR</button>
-        <button className="hover:bg-neutral-700 text-white px-4 py-2 rounded-sm text-sm transition duration-150 ease-in">EXPLORAR</button>
-        <button className="bg-green-800 text-white px-4 py-2 rounded-sm pr-8 pl-8 hover:bg-green-700 transition duration-150 ease-in">
-          <BiWallet size={20} />
-        </button>
-        <button className="bg-green-800 text-white px-4 py-2 rounded-sm pr-8 pl-8 transition hover:bg-green-700 duration-150 ease-in">
-          <BiCartAlt size={20} />
-        </button>
+      <div className="space-x-14 mr-7">
+        <button className="bg-[#09724A] text-white px-4 py-2 rounded-md pr-8 pl-8 transition hover:bg-green-700 duration-150 ease-in">Sign in</button>
       </div>
     </header>
   );
